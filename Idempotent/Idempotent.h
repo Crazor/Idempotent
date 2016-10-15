5175709d72286c03d20a1287600ce72b3c85e3ff
Crazor <crazor@gmail.com>
2016-10-16 00:35:34 +0200

    Initial Commit

---
+++ Idempotent/Idempotent.h
@@ -0,0 +1,19 @@
+//
+//  Idempotent.h
+//  Idempotent
+//
+//  Created by Crazor on 16.10.16.
+//  Copyright © 2016 Crazor. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Idempotent.
+FOUNDATION_EXPORT double IdempotentVersionNumber;
+
+//! Project version string for Idempotent.
+FOUNDATION_EXPORT const unsigned char IdempotentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Idempotent/PublicHeader.h>
+
+
